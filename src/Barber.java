89bb7320fc3d5c8c9c692d22967da1b6eb04f72f
Joseph <Joseph@192.168.1.143>
2015-10-25 17:38:07 -0700

    Stiff

---
+++ src/Barber.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joseph
+ */
+public class Barber extends Thread {
+    
+    private String name;
+    
+    public Barber(){
+        name = "bob";
+    }
+    public Barber(String n){
+        name = n;
+    }
+    
+    public void enterShop(){
+        
+    }
+    
+    public void cut(){
+        
+    }
+}
