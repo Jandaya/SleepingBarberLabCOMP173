77d61936f2e61a212dc44427e33fc0515a0dff79
DriftyS4 <j_andaya@u.pacific.edu>
2015-10-26 16:49:15 -0700

    Finished

--- src/BarberShop.java
+++ src/BarberShop.java
@@ -1,28 +1,25 @@
+/*
+Joseph Andaya
+COMP 173
+Lab 6 
+*/
 
+
+import java.awt.Color;
 import static java.lang.Thread.sleep;
 import java.util.concurrent.Semaphore;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Joseph
- */
 public class BarberShop extends javax.swing.JFrame {
 
     private int numSeats;
     private int seatsAvailable;
     private int numCustomers;
     private int customersInShop;
+    private int sleepSeconds;
     private Barber barber = new Barber();
     private Customer customer = new Customer();
-    private Semaphore barberSem = new Semaphore(0);
-    private Semaphore customerSem = new Semaphore(0);
-    private Semaphore seatsSem = new Semaphore(1);
+    private Semaphore barberSeatSem = new Semaphore(0);
+    private Semaphore waitingSeatSem = new Semaphore(0);
     
     public BarberShop() {
         initComponents();
@@ -31,43 +28,47 @@ public class BarberShop extends javax.swing.JFrame {
     public class Barber extends Thread {
     
     private String name;
+    private int barberNum;
     
     public Barber(){
-        name = "bob";
+        name = "Bob";
     }
     public Barber(String n){
         name = n;
     }
-    
+    // initialize barber
     public void enterBarberShop(){
         textArea.append("The Barber: " + name + " Arrives!");
         work();
     }
     
+    public void finishCustomer(){
+        // frees up the barber's seat
+        barberSeatSem.release(1);
+    }
     
     public void work(){
         while(numCustomers > 0){
             
+        // if all seats are available that means there are no customers, and the barber sleeps.    
         if(numSeats == seatsAvailable)
-                    barberStatus.setText("Barber Status: sleep");
+            barberStatus.setText("Barber Status: sleep");
         
             try{
-                customerSem.acquire();
-                
-                seatsSem.release();
-                barberSem.release();
-                seatsSem.release();
+                // get a customer
+                waitingSeatSem.acquire(1);
+
+                // perform work on customer
                 barberStatus.setText("Barber Status: cutting..");
+                finishCustomer();
+                suspendSleep();
+                
+                // once finished free up a seat
                 seatsAvailable++;
-                sleep(1000);
-                
                 seatsAvailableLabel.setText("Seats Available: " + seatsAvailable);
-
-                
-
             }
             catch (InterruptedException e){
-                    
+                    System.err.printf("Error on lock");
             }
         }
     }
@@ -81,7 +82,7 @@ public class BarberShop extends javax.swing.JFrame {
     public class Customer extends Thread {
     private int customerNum;
     private String customerName;
-    private boolean customerStatus = true;
+    private int customerStatus = 0;
     
     public Customer(){
         customerNum = 0;
@@ -96,53 +97,57 @@ public class BarberShop extends javax.swing.JFrame {
         // when customer leaves decrement
         customersInShop--;
         // customer is done, and leaves
-        customerStatus = false;
+        customerStatus = 1;
+        // update number of customers present on GUI
         customersPresentLabel.setText("Customers present: " + customersInShop);
+        
+        // if there are no customers in shop barber sleeps
+        if(customersInShop <= 0)
+                    barberStatus.setText("Barber Status: sleep");
     }
     
     public void enterBarberShop(){
+        // customers will sit down if they find a seat, if not they leave
         if(seatsAvailable > 0){
             sitDown();
         }
         else{
-            seatsSem.release();
+            //waitingChairsSem.release(1);
             textArea.append("\nNo seat for: "+ customerNum);
             leave();
         }
     }
     
-    public void sitDown(){
-        customerSem.release();
-        seatsSem.release();
-        seatsAvailable--;
-        seatsAvailableLabel.setText("Seats Available: " + seatsAvailable);
+    public void tryBarber(){
         try{
-            barberSem.acquire();
-            sleep(1000);
+            //Try to sit in barber's seat
+            barberSeatSem.acquire(1);
+            // gets haircut
+            suspendSleep();
+            // leaves shop, frees available seat
             leave();
         }
         catch (InterruptedException e){
-
+            System.err.printf("Error on lock");
         }
     }
     
+    public void sitDown(){
+        // increments the amount of customers coming in on semaphore
+        waitingSeatSem.release(1);
+        // decrement number of seats available in waiting room
+        seatsAvailable--;
+        seatsAvailableLabel.setText("Seats Available: " + seatsAvailable);
+        tryBarber();
+    }
+    
     public void run(){
-        while(customerStatus == true){
-            try{
-                seatsSem.acquire();
-                enterBarberShop();
-            }
-            catch (InterruptedException e){
-
-            }
+        // when a customer needs a haircut
+        while(customerStatus <= 0){
+            enterBarberShop();
         }
     }
 }
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -157,6 +162,9 @@ public class BarberShop extends javax.swing.JFrame {
         barberStatus = new javax.swing.JLabel();
         seatsAvailableLabel = new javax.swing.JLabel();
         customersPresentLabel = new javax.swing.JLabel();
+        sleepLabel = new javax.swing.JLabel();
+        sleepField = new javax.swing.JTextField();
+        errorLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -186,6 +194,12 @@ public class BarberShop extends javax.swing.JFrame {
 
         customersPresentLabel.setText("Customers Present: ");
 
+        sleepLabel.setText("Enter barber efficiency (seconds): ");
+
+        sleepField.setText("1");
+
+        errorLabel.setText("Positive numbers only!");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -196,13 +210,19 @@ public class BarberShop extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(openShopButton)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(numCustomersLabel)
-                        .addGap(18, 18, 18)
-                        .addComponent(numCustomersField, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(numSeatsLabel)
                         .addGap(18, 18, 18)
-                        .addComponent(numSeatsField, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(numSeatsField, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(numCustomersLabel)
+                            .addComponent(sleepLabel))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(errorLabel)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(numCustomersField, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+                                .addComponent(sleepField)))))
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
@@ -221,7 +241,13 @@ public class BarberShop extends javax.swing.JFrame {
                 .addComponent(seatsAvailableLabel)
                 .addGap(18, 18, 18)
                 .addComponent(customersPresentLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(errorLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(sleepLabel)
+                    .addComponent(sleepField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(numCustomersField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(numCustomersLabel))
@@ -239,32 +265,40 @@ public class BarberShop extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void openShopButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_openShopButtonActionPerformed
-        // TODO add your handling code here:
+        // initializes all values based on GUI Field
         numCustomers = Integer.parseInt(numCustomersField.getText());
         customersInShop = numCustomers;
         numSeats = Integer.parseInt(numSeatsField.getText());
         seatsAvailable = numSeats;
-        openBarberShop();
+        sleepSeconds = Integer.parseInt(sleepField.getText());
+        // input validation ensure they're positive and not 0
+        if (sleepSeconds <= 0 || numSeats <= 0 || numCustomers <= 0)
+            errorLabel.setForeground(Color.red);
+        else
+        {
+            errorLabel.setForeground(Color.black);
+            openBarberShop();
+        }
     }//GEN-LAST:event_openShopButtonActionPerformed
 
-    
+    // uses input from user for how long to sleep
+    public void suspendSleep(){
+        try {
+                sleep(1000 * sleepSeconds);
+            } catch(InterruptedException ex) {
+                    System.err.printf("Sleep Error.");
+            };
+    }
     public void openBarberShop(){
-        // start new thread for barber
-        Barber b = new Barber();
-        b.start();
         int i = 0;
+        // start new thread for barber
+        barber.start();
         
-        // create thread for each customer
+        // create thread for each customer and start action
         while(i < numCustomers){
-            
             Customer customer = new Customer(i);
             customer.start();
-            //customersPresentLabel.setText("Customers present: " + numCustomers);
             i++;
-            
-            try {
-                sleep(100);
-            } catch(InterruptedException ex) {};
         }
         
     }
@@ -272,7 +306,6 @@ public class BarberShop extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -295,7 +328,6 @@ public class BarberShop extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new BarberShop().setVisible(true);
@@ -306,6 +338,7 @@ public class BarberShop extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel barberStatus;
     private javax.swing.JLabel customersPresentLabel;
+    private javax.swing.JLabel errorLabel;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField numCustomersField;
     private javax.swing.JLabel numCustomersLabel;
@@ -313,6 +346,8 @@ public class BarberShop extends javax.swing.JFrame {
     private javax.swing.JLabel numSeatsLabel;
     private javax.swing.JButton openShopButton;
     private javax.swing.JLabel seatsAvailableLabel;
+    private javax.swing.JTextField sleepField;
+    private javax.swing.JLabel sleepLabel;
     private javax.swing.JTextArea textArea;
     // End of variables declaration//GEN-END:variables
 }
